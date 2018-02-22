49c629640f12a90fc7a0b50f647a6e8a277013c0
zhanjh <zhanjh@126.com>
2018-02-22 19:31:14 +0800

    Remove trait

--- src/DtoBase.php
+++ src/DtoBase.php
@@ -1,7 +1,62 @@
 <?php
 namespace Gap\Dto;
 
-class DtoBase implements \JsonSerializable
+abstract class DtoBase implements \JsonSerializable
 {
-    use MainTrait;
+    public function __construct(array $data = [])
+    {
+        $this->init();
+        $this->load($data);
+    }
+
+    public function jsonSerialize(): array
+    {
+        return get_object_vars($this);
+    }
+
+    public function getData(): array
+    {
+        return get_object_vars($this);
+    }
+
+    public function __set(string $key, $val): void
+    {
+        if ($flagPos = strpos($key, '_')) {
+            $subKey = substr($key, $flagPos + 1);
+
+            $dtoName = substr($key, 0, $flagPos);
+            if ($this->$dtoName) {
+                $this->$dtoName->$subKey = $val;
+                return;
+            }
+
+            // deprecated
+            $getDtoFun = 'get' . $dtoName;
+            if ($dto = $this->$getDtoFun()) {
+                $dto->$subKey = $val;
+                return;
+            }
+        }
+
+        throw new \OutOfBoundsException('Cannot find ' . $key . ' in ' . static::class);
+    }
+
+    public function load(array $data = []): void
+    {
+        foreach ($data as $key => $val) {
+            if (is_array($val) &&
+                property_exists($this, $key) &&
+                $this->$key instanceof self
+            ) {
+                $this->$key->load($val);
+                continue;
+            }
+
+            $this->$key = $val;
+        }
+    }
+
+    protected function init(): void
+    {
+    }
 }
